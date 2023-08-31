41ca95ce8fee6723664ab1b96e12b1e68d138c7b
Karol Kaźmierczak <kazmierczakkarol1999@gmail.com>
2023-08-31 22:33:38 +0200

    Add some tests on backend

---
+++ CargoApp/Modules/Locations/CargoApp.Modules.Locations.Tests/Queries/SearchLocationQueryTest.cs
@@ -0,0 +1,42 @@
+﻿using CargoApp.Core.Infrastructure.Response;
+using CargoApp.Modules.Locations.Application.DTO;
+using CargoApp.Modules.Locations.Application.ExternalServices.Locations;
+using CargoApp.Modules.Locations.Application.Queries.SearchLocations;
+using NSubstitute;
+using Xunit;
+
+namespace CargoApp.Modules.Locations.Tests.Queries;
+
+public class SearchLocationQueryTest
+{
+    private readonly ILocationClientFactory _locationClientFactory = Substitute.For<ILocationClientFactory>();
+    private readonly ILocationClient _locationClient = Substitute.For<ILocationClient>();
+    private readonly ISearchLocationQueryHandler _service;
+
+    public SearchLocationQueryTest()
+    {
+        _locationClientFactory.Create().Returns(_locationClient);
+
+        _service = new SearchLocationQueryHandler(_locationClientFactory);
+    }
+
+    [Fact]
+    public async Task ShouldCallSearchMethod()
+    {
+        // Arrange
+        var query = new SearchLocationQuery
+        {
+            Query = "QQuery"
+        };
+        _locationClient.Search(Arg.Any<string>(), Arg.Any<CancellationToken>())
+            .Returns(Result<IEnumerable<LocationDto>>.Success(new List<LocationDto>()));
+
+        // Act
+        var result = await _service.Handle(query);
+
+        // Arrange 
+        await _locationClient
+            .Received(1)
+            .Search(Arg.Is<string>(x => x == "QQuery"), Arg.Any<CancellationToken>());
+    }
+}
